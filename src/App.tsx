7c562796b33cc4860757a38f706cf9e8e62c0418
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-10-11 11:16:09 +0200

    feat: add routing using 'react-router-dom'

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,51 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './styles/App.css';
+import './styles/font.css';
+import './styles/index.css';
+import './styles/output.css';
+import './styles/input.css';
+
+// Global
+import Home from './pages/Home';
+import Analysis from './pages/Analysis';
+import Documentation from './pages/Documentation';
+import NotFound from './pages/NotFound';
+
+// Authentication
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+
+// Connected
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Router>
+        <Routes>
+          {/* Global */}
+          <Route path="/" element={<Home />} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/documentation" element={<Documentation />} />
+
+          {/* Authentication */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+
+          {/* Connected */}
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* 404 */}
+          {/* Ne fonctionnera pas totalement, il faut adapter la config en fonction d'ou on héberge le site */}
+          <Route path="/404" element={<NotFound />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
