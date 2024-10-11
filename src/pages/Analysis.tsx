2d2b9588269b4507980fb5faa9cce31f4ffdad4a
Meymoun Boualaoui <77726988+MeymounB@users.noreply.github.com>
2024-10-11 11:23:44 +0200

    chore: architecture for pages folder

---
+++ src/pages/Analysis.tsx
@@ -0,0 +1,5 @@
+const Analysis = () => {
+  return <>Page Analysis</>;
+};
+
+export default Analysis;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import HeroSection from '../components/Sections/HeroSection/HeroSection';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+export default Home;
