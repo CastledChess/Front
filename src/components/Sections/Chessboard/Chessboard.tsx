dbb0b9343c5a046c175cd9c31fa7078f11b7e410
Lxdovic <ludovicdebever0@gmail.com>
2024-10-24 15:18:09 +0200

    improv(chessboard): use chessground instead of wrapper
    The wrapper was very old, outdated and had barely any code, doing it this way is better

--- src/components/Sections/Chessboard/Chessboard.tsx
+++ src/components/Sections/Chessboard/Chessboard.tsx
@@ -1,23 +1,49 @@
-import Chessground from '@react-chess/chessground';
+import { Chessground } from 'chessground';
+import { useLayoutEffect, useMemo, useRef } from 'react';
+import { Api } from 'chessground/api';
+import { Chess } from 'chess.js';
+import { Config } from 'chessground/config';
 
 import './base.css';
 import './brown.css';
 import './cburnett.css';
 import './3d.css';
 
-type ChessboardProps = {
-  width: number;
-  height: number;
-};
+export const Chessboard = () => {
+  const chess = useRef(new Chess('r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19'));
+  const chessGroundRef = useRef<HTMLDivElement | null>(null);
+  const chessGround = useRef<Api>();
 
-export const Chessboard = ({ width, height }: ChessboardProps) => {
-  return (
-    <Chessground
-      width={width}
-      height={height}
-      config={{
-        addPieceZIndex: true,
-      }}
-    />
+  const onMove = (origin: string, destination: string) => {
+    try {
+      chess.current.move({
+        from: origin,
+        to: destination,
+      });
+    } catch (err) {
+      console.error(err);
+      chessGround.current?.cancelMove();
+      chessGround.current?.set({
+        fen: chess.current.fen(),
+      });
+    }
+  };
+
+  const CHESSGROUND_CONFIG: Config = useMemo(
+    () => ({
+      fen: chess.current.fen(),
+      events: {
+        move: onMove,
+      },
+    }),
+    [],
   );
+
+  useLayoutEffect(() => {
+    if (!chessGroundRef.current) return;
+
+    chessGround.current = Chessground(chessGroundRef.current, CHESSGROUND_CONFIG);
+  }, []);
+
+  return <div ref={chessGroundRef} className="w-full h-full aspect-square" />;
 };
