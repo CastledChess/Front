9bbdd56cd5b9f70dbdf6a59cdb3b91bd412d934c
Lxdovic <ludovicdebever0@gmail.com>
2024-10-24 11:24:32 +0200

    feat(analysis): create basic chessboard

--- src/components/Sections/Chessboard/Chessboard.tsx
+++ src/components/Sections/Chessboard/Chessboard.tsx
@@ -1,7 +1,23 @@
-export default function Chessboard() {
+import Chessground from '@react-chess/chessground';
+
+import './base.css';
+import './brown.css';
+import './cburnett.css';
+import './3d.css';
+
+type ChessboardProps = {
+  width: number;
+  height: number;
+};
+
+export const Chessboard = ({ width, height }: ChessboardProps) => {
   return (
-    <div id="chessboard" className="">
-      Chessboard
-    </div>
+    <Chessground
+      width={width}
+      height={height}
+      config={{
+        addPieceZIndex: true,
+      }}
+    />
   );
-}
+};
