9e3b1df1f6a26fc94334b04abc666fe44fe49f31
Lxdovic <ludovicdebever0@gmail.com>
2024-10-24 11:26:09 +0200

    refactor: change router, tailwind config and css
    - Added a navbar component
    - Added a container for pages in router
    - Modified Tailwind config -> tailwind now works

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './styles/App.css';
 import './styles/font.css';
 import './styles/index.css';
-import './styles/output.css';
-import './styles/input.css';
 
 // Global
 import Home from './pages/Home';
-import Analysis from './pages/Analysis';
+import Analysis from './pages/Analysis/Analysis.tsx';
 import Documentation from './pages/Documentation';
 import NotFound from './pages/NotFound';
 
@@ -20,34 +16,38 @@ import Logout from './pages/Logout';
 
 // Connected
 import Dashboard from './pages/Dashboard';
+import { Navbar } from './components/Navbar/Navbar.tsx';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          {/* Global */}
-          <Route path="/" element={<Home />} />
-          <Route path="/analysis" element={<Analysis />} />
-          <Route path="/documentation" element={<Documentation />} />
+    <main>
+      <Navbar />
+      <div className="h-[calc(100vh-3.5rem)] p-20">
+        <Router>
+          <Routes>
+            {/* Global */}
+            <Route path="/" element={<Home />} />
+            <Route path="/analysis" element={<Analysis />} />
+            <Route path="/documentation" element={<Documentation />} />
 
-          {/* Authentication */}
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
+            {/* Authentication */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
 
-          {/* Connected */}
-          <Route path="/dashboard" element={<Dashboard />} />
+            {/* Connected */}
+            <Route path="/dashboard" element={<Dashboard />} />
 
-          {/* 404 */}
-          {/* Ne fonctionnera pas totalement, il faut adapter la config en fonction d'ou on héberge le site */}
-          <Route path="/404" element={<NotFound />} />
-          <Route path="/not-found" element={<NotFound />} />
-          <Route path="/notfound" element={<NotFound />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+            {/* 404 */}
+            {/* Ne fonctionnera pas totalement, il faut adapter la config en fonction d'ou on héberge le site */}
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/not-found" element={<NotFound />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </div>
+    </main>
   );
 }
 
