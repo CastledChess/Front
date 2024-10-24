1bfe6c68eee9137c20778073c16d2e92cfa1589d
Lxdovic <ludovicdebever0@gmail.com>
2024-10-24 15:18:09 +0200

    improv(chessboard): use chessground instead of wrapper
    The wrapper was very old, outdated and had barely any code, doing it this way is better

--- src/pages/Analysis/Analysis.tsx
+++ src/pages/Analysis/Analysis.tsx
@@ -3,7 +3,9 @@ import { Chessboard } from '../../components/Sections/Chessboard/Chessboard.tsx'
 const Analysis = () => {
   return (
     <div className="w-full h-full flex gap-20">
-      <Chessboard width={700} height={700} />
+      <div className="h-full">
+        <Chessboard />
+      </div>
     </div>
   );
 };
