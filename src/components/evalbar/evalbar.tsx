eabe80bd671fbe7a77e75a6a4c7401d7ea7d46d8
Lxdovic <ludovicdebever0@gmail.com>
2024-10-31 14:06:22 +0100

    fix(analysis): fix move classification algorithm

--- src/components/evalbar/evalbar.tsx
+++ src/components/evalbar/evalbar.tsx
@@ -2,16 +2,22 @@ import { motion } from 'framer-motion';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip.tsx';
 
 type EvalbarProps = {
+  orientation: 'white' | 'black';
   winChance: number;
   evaluation: number;
 };
 
-export const Evalbar = ({ winChance, evaluation }: EvalbarProps) => {
+export const Evalbar = ({ orientation, winChance, evaluation }: EvalbarProps) => {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={100}>
         <TooltipTrigger asChild>
-          <div className="relative flex flex-col h-full overflow-hidden w-10 rounded-lg">
+          <div
+            className="relative flex flex-col h-full overflow-hidden w-10 rounded-lg"
+            style={{
+              transform: orientation === 'black' ? 'rotate(180deg)' : 'rotate(0deg)',
+            }}
+          >
             <motion.div
               animate={{ height: `${100 - winChance}%` }}
               initial={{ height: '50%' }}
