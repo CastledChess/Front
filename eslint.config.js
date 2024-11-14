5b9f16274b421abf1452df14484167db16e613de
J-Babin <babin.jeremy29@gmail.com>
2024-11-14 15:08:58 +0100

    feat(tailwindconfig): add colors variable.

--- eslint.config.js
+++ eslint.config.js
@@ -7,16 +7,22 @@ export default [
   { files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: { ...globals.browser, React: 'writable' } } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
 
   {
     ...pluginReact.configs.flat.recommended,
     rules: {
       'react/prop-types': 'off',
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
     },
     settings: {
       react: {
         version: 'detect',
+      },
+      plugins: {
+        // Add the react plugin
+        pluginReact,
       },
     },
   },
