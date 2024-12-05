501005de8c180ebd5254a5d387467d97170cd86f
Florian Autour <florian.autour@epitech.eu>
2024-12-05 12:13:57 +0100

    fix(i18n): remove 'Username' translation

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -63,7 +63,7 @@ export const Profile = () => {
         <Pencil className="absolute bottom-0 right-0 bg-transparent text-castled-btn-primary -translate-y-6" />
       </div>
       <Card className="w-full max-w-md p-4">
-        <h1 className="text-castled-accent text-4xl my-8 mx-14 text-center">{t('Username')}</h1>
+        <h1 className="text-castled-accent text-4xl my-8 mx-14 text-center">Username</h1>
         <div className="my-4 mx-14">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-3">
