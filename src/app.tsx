25c0517a993de11b8387fc78e376d6fe8bc815b8
Inês <ines-sofia.simoes-rodrigues-bretes@epitech.eu>
2024-12-18 18:13:26 +0100

    feat(history): fix redirection

--- src/app.tsx
+++ src/app.tsx
@@ -61,7 +61,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
 
             <Route
-              path="/history"
+              path="/dashboard"
               element={
                 <ProtectedRoute allow={!!user} redirect="/login">
                   <History />
