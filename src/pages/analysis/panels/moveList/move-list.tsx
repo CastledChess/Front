0f3fabf814bd2777403eeccf2abd7e37346b9440
Ludovic <ludovicdebever0@gmail.com>
2024-12-22 19:56:07 +0100

    feat(analysis): added move list

--- src/pages/analysis/panels/moveList/move-list.tsx
+++ src/pages/analysis/panels/moveList/move-list.tsx
@@ -1,8 +1,79 @@
-﻿import { Icon } from '@iconify/react';
+﻿import { useAnalysisStore } from '@/store/analysis.ts';
+import { Button } from '@/components/ui/button';
+import { Color } from 'chess.js';
+import {
+  classificationToTailwindColor,
+  shouldDisplayClassificationInMoveHistory,
+} from '@/pages/analysis/classifications.ts';
+import { cn } from '@/lib/utils.ts';
 
-export const MoveList = () => (
-  <div className="flex h-full items-center justify-center gap-6 p-6">
-    <Icon icon="ix:move" width={30} />
-    <span className="font-semibold">Move List</span>
-  </div>
-);
+export const MoveList = () => {
+  const { analysis, chess, chessGround, setCurrentMove } = useAnalysisStore();
+
+  const handleSkipToMove = (index: number) => {
+    const moves = chess.history();
+
+    for (let i = 0; i < moves.length; i++) {
+      chess.undo();
+    }
+
+    for (let i = 0; i < index; i++) {
+      chess.move(analysis!.moves[i].move);
+    }
+
+    chessGround?.set({
+      fen: chess.fen(),
+    });
+    setCurrentMove(index);
+    chessGround?.redrawAll();
+  };
+
+  const toPieceNotation = (move: string, color: Color): string => {
+    if (color === 'w') {
+      move = move.replace('N', '♞');
+      move = move.replace('B', '♝');
+      move = move.replace('R', '♜');
+      move = move.replace('Q', '♛');
+      move = move.replace('K', '♚');
+      move = move.replace('P', '♟');
+
+      return move;
+    }
+
+    move = move.replace('N', '♘');
+    move = move.replace('B', '♗');
+    move = move.replace('R', '♖');
+    move = move.replace('Q', '♕');
+    move = move.replace('K', '♔');
+    move = move.replace('P', '♙');
+
+    return move;
+  };
+
+  return (
+    <div className="flex">
+      <div className="flex gap-1 p-6 flex-wrap">
+        {analysis?.moves.map((move, index) => (
+          <Button
+            key={index}
+            className="flex gap-2 p-1 h-max"
+            variant="ghost"
+            onClick={() => handleSkipToMove(index + 1)}
+          >
+            {index % 2 == 0 && <span>{Math.floor(index / 2) + 1}.</span>}
+            <span
+              className={cn(
+                'text-sm',
+                move.classification &&
+                  shouldDisplayClassificationInMoveHistory[move.classification] &&
+                  classificationToTailwindColor[move.classification],
+              )}
+            >
+              {toPieceNotation(move.move.san, move.move.color)}
+            </span>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
