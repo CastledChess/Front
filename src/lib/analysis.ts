5597b0247b9395c7f87c9243367790e50ecc502f
Ludovic <ludovicdebever0@gmail.com>
2024-12-24 17:21:45 +0100

    fix(classification): fix classification

--- src/lib/analysis.ts
+++ src/lib/analysis.ts
@@ -4,7 +4,6 @@ import { StartAnalysisFormSchema } from '@/schema/analysis.ts';
 import { Move } from 'chess.js';
 import { StockfishService } from '@/services/stockfish/stockfish.service.ts';
 import { UciParserService } from '@/services/stockfish/uci-parser.service.ts';
-import { pieceToValue } from '@/data/classifications.ts';
 import { isCached } from '@/services/cache/cache.service.ts';
 
 export type AnalyseMovesLocalParams = {
@@ -113,22 +112,19 @@ export const classifyRegular = (move: AnalysisMove, index: number, moves: Analys
   if (!next || !current) return { ...move, classification: AnalysisMoveClassification.None };
 
   if (current.mate)
-    return { ...move, classification: classifyWithMate(move.move, next.mate || current.mate, current.mate) };
-  else return { ...move, classification: classifyWithWinChance(move.move, next.winChance!, current.winChance!) };
+    return { ...move, classification: classifyWithMate(move.move, next, current) };
+  else return { ...move, classification: classifyWithWinChance(move.move, next, current) };
 };
 
-const classifyWithMate = (move: Move, next: number, current: number): AnalysisMoveClassification => {
-  if (next === null || current === null) return AnalysisMoveClassification.None;
+const classifyWithMate = (move: Move, next:  InfoResult, current:  InfoResult): AnalysisMoveClassification => {
+  if (next.mate === null || current.mate === null) return AnalysisMoveClassification.None;
 
-  const mateDelta = next - current - 1;
+  const mateDelta = (next.mate || current.mate) - current.mate - 1;
 
   let classification = AnalysisMoveClassification.None;
 
-  if (mateDelta <= 0) {
-    if (move.captured && pieceToValue[move.captured] < pieceToValue[move.piece])
-      classification = AnalysisMoveClassification.Brilliant;
-    else classification = AnalysisMoveClassification.Best;
-  } else if (mateDelta === 1) classification = AnalysisMoveClassification.Excellent;
+  if (move.from + move.to === current.move) classification = AnalysisMoveClassification.Best;
+  else if (mateDelta <= 0) classification = AnalysisMoveClassification.Excellent;
   else if (mateDelta === 2) classification = AnalysisMoveClassification.Good;
   else if (mateDelta === 3) classification = AnalysisMoveClassification.Inaccuracy;
   else if (mateDelta === 4) classification = AnalysisMoveClassification.Mistake;
@@ -137,18 +133,15 @@ const classifyWithMate = (move: Move, next: number, current: number): AnalysisMo
   return classification;
 };
 
-const classifyWithWinChance = (move: Move, next: number, current: number): AnalysisMoveClassification => {
-  if (!next || !current) return AnalysisMoveClassification.None;
+const classifyWithWinChance = (move: Move, next:  InfoResult, current:  InfoResult): AnalysisMoveClassification => {
+  if (!next.winChance  || !current.winChance) return AnalysisMoveClassification.None;
 
-  const winChanceDelta = Math.abs((current - next) / 100);
+  const winChanceDelta = Math.abs((current.winChance - next.winChance) / 100);
 
   let classification = AnalysisMoveClassification.None;
 
-  if (winChanceDelta <= 0.0) {
-    if (move.captured && pieceToValue[move.captured] < pieceToValue[move.piece])
-      classification = AnalysisMoveClassification.Brilliant;
-    else classification = AnalysisMoveClassification.Best;
-  } else if (winChanceDelta > 0.0 && winChanceDelta <= 0.02) classification = AnalysisMoveClassification.Excellent;
+  if (move.from + move.to === current.move) classification = AnalysisMoveClassification.Best;
+  else if (winChanceDelta > 0.0 && winChanceDelta <= 0.02) classification = AnalysisMoveClassification.Excellent;
   else if (winChanceDelta > 0.02 && winChanceDelta <= 0.05) classification = AnalysisMoveClassification.Good;
   else if (winChanceDelta > 0.05 && winChanceDelta <= 0.1) classification = AnalysisMoveClassification.Inaccuracy;
   else if (winChanceDelta > 0.1 && winChanceDelta <= 0.2) classification = AnalysisMoveClassification.Mistake;
