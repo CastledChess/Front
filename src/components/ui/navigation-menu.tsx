4c16608edf5c0a653da0c631500b7108df72adc5
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2025-02-08 20:20:52 +0100

    Improve/navbar (#97)
    * style(navbar): homogenize with the rest of the ui
    
    * style(navbar): homogenize with the rest of the ui

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -35,7 +35,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  'hover:underline group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50',
+  'hover:underline hover:bg-castled-secondary group inline-flex h-8 w-max items-center justify-center rounded-md p-3 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50',
 );
 
 const NavigationMenuTrigger = React.forwardRef<
