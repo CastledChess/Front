c7afd17f3a5fdd0f2a66b1d4262d408b63a39625
Ludovic <ludovicdebever0@gmail.com>
2025-02-25 21:13:03 +0100

    fix(elitedb): fix elitedb move entity and cleanup

--- src/api/database.ts
+++ src/api/database.ts
@@ -1,6 +1,5 @@
-// import { apiAuthenticated } from '@/api/index.ts';
-import axios from 'axios';
+import { apiAuthenticated } from '@/api/index.ts';
 
 export const queryPosition = async (fen: string) => {
-  return await axios.get(`http://127.0.0.1:3001/pos?fen=${fen}`);
+  return await apiAuthenticated.get(`/api/v1/elitedb?fen=${fen}`);
 };
