3c75de2271bb1c869a5b5b530603dd13dabb89bd
Ludovic <ludovicdebever0@gmail.com>
2025-02-26 13:46:17 +0100

    improve(db): add caching

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@/components/theme-provider.tsx';
 import App from './app.tsx';
 import '@/i18n.config.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
 
