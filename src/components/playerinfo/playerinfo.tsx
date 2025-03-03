7d24574f7b2df5f71907931a0ec8d093eba21bae
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2025-03-03 18:01:18 +0100

    improve(controls): move controls inside of the chessboard panel (#140)
    - Simplifies UI and gains space

--- src/components/playerinfo/playerinfo.tsx
+++ src/components/playerinfo/playerinfo.tsx
@@ -8,7 +8,7 @@ export const PlayerInfo = ({ player }: PlayerInfoProps) => {
   if (!player.username) return null;
 
   return (
-    <div className="flex gap-2 items-center h-6 lg:h-10">
+    <div className="flex gap-2 max-w-1/2 items-center h-6 lg:h-10">
       <Avatar className="lg:w-10 lg:h-10 w-7 h-7 lg:text-xl text-xs">
         <AvatarFallback>{player.username[0].toUpperCase()}</AvatarFallback>
       </Avatar>
--- src/pages/analysis/panels/chessboard/chessboard-panel.tsx
+++ src/pages/analysis/panels/chessboard/chessboard-panel.tsx
@@ -2,6 +2,7 @@
 import { PlayerInfo } from '@/components/playerinfo/playerinfo.tsx';
 import { AnalysisChessboard } from '@/components/chessboard/analysis-chessboard.tsx';
 import { useAnalysisStore } from '@/store/analysis.ts';
+import { Controls } from '@/pages/analysis/panels/controls/controls.tsx';
 import './chessboard-panel.css';
 
 /**
@@ -28,7 +29,7 @@ export const ChessboardPanel = () => {
   const { analysis, currentMove, orientation } = useAnalysisStore();
 
   return (
-    <div className="flex h-full w-full items-center justify-center p-0 lg:p-6 relative chessboard-panel">
+    <div className="flex h-full w-full items-center justify-center p-0 lg:p-6 relative">
       <div className="h-full flex flex-col w-full">
         <div className="flex gap-2 lg:gap-6 items-center justify-center flex-grow relative chessboard-panel-inner">
           <div className="eval-bar-container">
@@ -42,7 +43,7 @@ export const ChessboardPanel = () => {
 
           <div className="chessboard-container aspect-square relative">
             {analysis && (
-              <div className="absolute top-0 -translate-y-[calc(100%+.5rem)]">
+              <div className="mb-2">
                 <PlayerInfo
                   player={{
                     username: orientation === 'white' ? analysis.header.Black : analysis.header.White,
@@ -53,13 +54,16 @@ export const ChessboardPanel = () => {
             )}
             <AnalysisChessboard />
             {analysis && (
-              <div className="absolute top-[calc(100%+.5rem)]">
+              <div className="mt-2 relative">
                 <PlayerInfo
                   player={{
                     username: orientation === 'white' ? analysis.header.White : analysis.header.Black,
                     rating: orientation === 'white' ? analysis.header.WhiteElo : analysis.header.BlackElo,
                   }}
                 />
+                <div className="lg:flex hidden absolute top-0 w-1/2 right-0">
+                  <Controls />
+                </div>
               </div>
             )}
           </div>
